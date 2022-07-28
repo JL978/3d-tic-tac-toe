9b0a681757d6c20e192c31ed1bcb2debf18acda7
Jimmy Lam <JimmyLam978@gmail.com>
2022-07-28 00:34:43 -0400

    moved text a little

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ const CustomOrbitControls: React.FC<{
                 ref={textRef}
                 color={'#bdfff0'}
                 scale={[30, 30, 30]}
-                position={[9, winner ? 28 : 26, 9]}
+                position={[9, 28, 9]}
                 anchorX="center">
                 {winner ? 'Winner: ' + winner : 'Turn: ' + turn}
             </Text>
