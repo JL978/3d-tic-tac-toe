8abd73fc4b9528dd912e0836535edad5a16d1cf2
Jimmy Lam <JimmyLam978@gmail.com>
2023-01-07 17:14:03 -0500

    added pwa?

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,47 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()]
+    plugins: [
+        react(),
+        svgr(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            includeAssets: ['/assets/favicon.ico', '/assets/apple-touch-icon.png'],
+            manifest: {
+                name: '3d Tic Tac Toe',
+                short_name: '3DTTT',
+                description: 'A 3D Tic Tac Toe game',
+                theme_color: '#061626',
+                icons: [
+                    {
+                        src: '/assets/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/assets/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/assets/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/assets/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ]
 });
